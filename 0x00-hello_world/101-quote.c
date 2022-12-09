3a7ea27c32abccc279d59d90893e424ff862b479
Benjamin-yankey <frodoandimaro0@gmail.com>
2022-12-08 16:19:33 -0800

    I love you

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 /**
- * main - Entry point 
+ * main - Entry print exactly and that piece of art is useful
  *
  * Return: Always 1 (Success)
  */
@@ -11,9 +10,6 @@
 int main(void)
 
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
