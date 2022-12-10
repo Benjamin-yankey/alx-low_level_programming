f6fbc9f1b08e151e76e77e7b7df7507178800d6d
Benjamin-yankey <frodoandimaro0@gmail.com>
2022-12-09 19:21:02 -0800

    capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char s;
+
+	char d;
+
+	s = 'a';
+	d = 'A';
+	while 
+		(s <= 'z'){
+			putchar(s);
+			s++;
+		}
+	while 
+		(d <= 'Z'){
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
