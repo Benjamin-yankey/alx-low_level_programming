bf724bc87bc6eec5ac447c1791ce2a63022d88d5
Benjamin-yankey <frodoandimaro0@gmail.com>
2022-12-09 19:44:28 -0800

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while 
+		(m <= 'z') {
+			if ((m != 'q' || m != 'e') && m <= 'z')
+				putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
