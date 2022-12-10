00f3cac37f4331e84eea9f422647b24dfedd329e
Benjamin-yankey <frodoandimaro0@gmail.com>
2022-12-09 19:00:23 -0800

    alphabeth

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'z') 
-	{
-		putchar(ch);
-		ch++;
+	while 
+		(ch <= 'z') {
+			putchar(ch);
+			ch++;
 	}
 	putchar('\n');
-
 	return (0);
 }
