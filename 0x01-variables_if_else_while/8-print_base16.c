167063f28cb4ffbef2406b64117e3abb3509f353
Benjamin-yankey <frodoandimaro0@gmail.com>
2022-12-09 21:32:09 -0800

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char h;
+
+	int d;
+
+	h = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while 
+		(h <= 'f') {
+			putchar(h);
+			h++;
+		}
+	putchar('\n');
+	return (0);
+}
