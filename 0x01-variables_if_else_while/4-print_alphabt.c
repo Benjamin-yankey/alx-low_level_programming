0102660e11d21e32d2726b577d100c8ff9681b14
Benjamin-yankey <frodoandimaro0@gmail.com>
2022-12-12 15:45:16 -0800

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	char c;
+	char ch;
 
-	c = 'a';
-	while
-		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'z')
-			        putchar(c);
-			c++;
-		}
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
 	putchar('\n');
+
 	return (0);
 }
