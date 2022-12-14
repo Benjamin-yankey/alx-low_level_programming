7ad82ad6ac5c804fdded434f7fe6bcaa57385d90
Benjamin-yankey <frodoandimaro0@gmail.com>
2022-12-13 18:13:02 -0800

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,12 @@ void print_alphabet_x10(void)
 	int num;
 	char ch;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 1; num <= 10; num++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
-
 		_putchar('\n');
 	}
-
 }
